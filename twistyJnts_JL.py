2b4a8768e5ad738a8e96d671d5158187f5f29f07
Jacob Lilly <jacobjlilly@berkeley.edu>
2020-09-07 16:40:00 -0700

    Update twistyJnts_JL.py
    now names things nicely!

--- twistyJnts_JL.py
+++ twistyJnts_JL.py
@@ -5,29 +5,32 @@ ex: select shoulder then elbow and specify 3 to create
 3 intermediate joints
 """
 
-twistNum = 3 #user picks number of twisty joints to add
-n = twistNum + 2
+n = 3 #user picks number of twisty joints to add
 start, end = ls(sl=1)
 
 twists = []
 select(start)
-while n > 0:
-    jnt = joint(n=start) #might have to name better
-    if n == twistNum + 2:
-        startTwist = jnt
+for i in range(n+2):
+    jnt = joint(rad = .01)
+    jnt = rename(jnt, start.replace('_jnt', 'Twist' + str(i) + '_jnt'))
     twists.append(jnt)
-    if n == 1:
+    if i == 0:
+        startTwist = jnt
+    if i == n:
         delete(parentConstraint(end, jnt))
         endTwist = jnt
-    n -= 1
 
 # evenly space out twisties
-tx = getAttr(endTwist + '.tx') / (twistNum + 2)
+tx = getAttr(end + '.tx') / (n+1)
 for twist in twists[1:]:
     setAttr(twist + '.tx', tx)
 
 # make IK spline and connect twist
-hdl = ikHandle(sj=startTwist, ee=endTwist, sol = 'ikSplineSolver')[0]
+hdl, eff, crv = ikHandle(sj=startTwist, ee=endTwist, sol = 'ikSplineSolver')
+hdl = rename(hdl, start.replace('_jnt', 'Twist_hdl'))
+eff = rename(eff, start.replace('_jnt', 'Twist_eff'))
+crv = rename(crv, start.replace('_jnt', 'Twist_crv'))
+hide(hdl, eff, crv)
 mult = shadingNode('multiplyDivide', asUtility=1)
 connectAttr(end + '.rx', mult + '.input1X')
 connectAttr(mult + '.outputX', hdl + '.twist')
